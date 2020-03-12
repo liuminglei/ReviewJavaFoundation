81fb7bba2af7d7a4d08090ecc09b16e0390ddaf7
liuminglei200@163.com <liu19872008>
2020-03-12 11:16:21 +0800

    重温Java基础（二）：变量

---
+++ 02/Variable/src/main/java/com/luas/javafoundation/ErrorVariableDefine.java
@@ -0,0 +1,11 @@
+package com.luas.javafoundation;
+
+public class ErrorVariableDefine {
+
+    public static void main(String[] args) {
+        int i;
+    // 放开此行点，即会出现编译错误
+//        System.out.println(i);
+    }
+
+}
---
+++ 02/Variable/src/main/java/com/luas/javafoundation/VariableApp.java
@@ -0,0 +1,13 @@
+package com.luas.javafoundation;
+
+public class VariableApp {
+
+    public static void main(String[] args) {
+        int i;
+
+        i = 9;
+
+        int j = 9;
+    }
+
+}
