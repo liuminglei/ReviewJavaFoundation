9a6e7cbe1cd7c824537581e269d3ee66e216bf88
liuminglei200@163.com <liu19872008>
2020-03-12 13:46:33 +0800

    重温Java基础（三）：常量

---
+++ 03/Constants/src/main/java/com/luas/javafoundation/App.java
@@ -0,0 +1,18 @@
+package com.luas.javafoundation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+
+    public static void main( String[] args )
+    {
+        final double PI = 3.1415926D;
+
+        System.out.println( "Hello World!" );
+
+        System.out.println("π的值为：" + PI);
+    }
+}
---
+++ 03/Constants/src/main/java/com/luas/javafoundation/common/Constants.java
@@ -0,0 +1,9 @@
+package com.luas.javafoundation.common;
+
+public class Constants {
+
+    public static final double PI = 3.2415926d;
+
+    public static final double CM_PER_INCH = 2.54d;
+
+}
